95786ce9bb8c56a9eef0177e642fed954289dc6f
Sam Aaron <samaaron@gmail.com>
2012-07-07 00:28:53 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject mini-beast "1.0.0-SNAPSHOT"
+(defproject mini-beast "0.0.1-SNAPSHOT"
   :description "mini-beast"
   :dependencies [[org.clojure/clojure "1.4.0"]
-      [overtone "0.7.1"]
-      [quil "1.4.1"]
-      [commons-collections "3.0-dev2"]]
+                 [overtone "0.8.0-SNAPSHOT"]
+                 [quil "1.6.0"]
+                 [commons-collections "3.0-dev2"]]
   :main minibeast.core
-  ;;:main overtone.live
-  :repl-init minibeast.core
-)
+  :repl-init minibeast.core)
